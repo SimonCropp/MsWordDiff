7e05825b92b8ab24d2c4083fe40f044a41697ed4
Simon Cropp <simon.cropp@gmail.com>
2025-12-16 12:09:03 +1100

    .

--- src/Tests/Test.cs
+++ src/Tests/Test.cs
@@ -3,5 +3,7 @@
     [Test]
     [Explicit]
     public void Launch() =>
-        Program.Run(ProjectFiles.input_temp_docx, ProjectFiles.input_target_docx);
+        Word.Launch(
+            ProjectFiles.input_temp_docx.FullPath,
+            ProjectFiles.input_target_docx.FullPath);
 }
